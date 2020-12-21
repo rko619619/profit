c1891360e7366e6f61681411a2139900d71e0551
Yanis Romanovich <thrasherdota2@gmail.com>
2020-12-22 00:34:40 +0300

    Fixed

--- scraper.rb
+++ scraper.rb
@@ -3,18 +3,19 @@ require 'nokogiri'
 require 'csv'
 require 'time'
 
+time1 = Time.now
 url_category = "http://loveread.ec/index_book.php?id_genre=1&p="
 global = []
-(1..1).each do |page|
+(1..1550).each do |page|
   noke = Nokogiri::HTML(Curl::Easy.http_get("#{url_category.to_s}#{page}").body_str)
-  (1..1).each do |number|
+  (1..6).each do |number|
     name = noke.xpath("//table[#{number}][@class='table_gl']//div[@class='td_top_text']").text
     genre = noke.xpath("//table[#{number}][@class='table_gl']//tr[@class='td_top_color']//td[1]//p").text
     author = noke.xpath("//table[#{number}][@class='table_gl']//td[@class='span_str']//a[2]//strong").text
     description = noke.xpath("//table[#{number}][@class='table_gl']//tr[3][@class='td_center_color']//td/p").text
     publishing = noke.xpath("//table[#{number}][@class='table_gl']//td[@class='span_str']//p/text()").text
     general = publishing.delete("\t\r").strip!.squeeze("\n").split("\n")
-
+    image = 'http://loveread.ec/' + noke.xpath("//table[#{number}][@class='table_gl']//img[@class='margin-right_8']").attr('src')
     global << [
        name,
        genre,
@@ -25,9 +26,10 @@ global = []
        general[3],
        general[4],
        general[5],
-       description
+       description,
+       image
     ]
   end
 end
-print(global)
-
+time2 = Time.now
+puts(time2 - time1)

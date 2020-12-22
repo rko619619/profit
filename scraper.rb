5b7317e58c34c0073dc3b16cf9bc5d20922043d9
Yanis Romanovich <thrasherdota2@gmail.com>
2020-12-22 22:18:29 +0300

    Added a OOP

--- scraper.rb
+++ scraper.rb
@@ -3,42 +3,83 @@ require 'nokogiri'
 require 'csv'
 require 'time'
 
-time1 = Time.now
-url_category = "http://loveread.ec/index_book.php?id_genre=1&p="
-global = []
-threads = []
-mutex = Mutex.new
-(1..1550).each do |page|
-  threads << Thread.new(page) do |page_html|
-    noke = Nokogiri::HTML(Curl::Easy.http_get("#{url_category.to_s}#{page_html}").body_str)
-    (1..6).each do |number|
-      name = noke.xpath("//table[#{number}][@class='table_gl']//div[@class='td_top_text']").text
-      genre = noke.xpath("//table[#{number}][@class='table_gl']//tr[@class='td_top_color']//td[1]//p").text
-      author = noke.xpath("//table[#{number}][@class='table_gl']//td[@class='span_str']//a[2]//strong").text
-      description = noke.xpath("//table[#{number}][@class='table_gl']//tr[3][@class='td_center_color']//td/p").text
-      publishing = noke.xpath("//table[#{number}][@class='table_gl']//td[@class='span_str']//p/text()").text
-      general = publishing.delete("\t\r").strip!.squeeze("\n").split("\n")
-      image = 'http://loveread.ec/' + noke.xpath("//table[#{number}][@class='table_gl']//img[@class='margin-right_8']").attr('src')
-      mutex.synchronize{
-        global << [
-            name,
-            genre,
-            author,
-            general[0],
-            general[1],
-            general[2],
-            general[3],
-            general[4],
-            general[5],
-            description,
-            image
-        ]
-      }
-    end
+url = 'http://loveread.ec/index_book.php?id_genre=1&p='
+
+class Download
+  def self.get_html(url, page_html)
+    Nokogiri::HTML(Curl::Easy.http_get("#{url}#{page_html}").body_str)
   end
-  threads.each { |aThread| aThread.join }
+
+  def self.get_name(document, number)
+    document.xpath("//table[#{number}][@class='table_gl']//div[@class='td_top_text']").text
+  end
+
+  def self.get_genre(document, number)
+    document.xpath("//table[#{number}][@class='table_gl']//tr[@class='td_top_color']//td[1]//p").text
+  end
+
+  def self.get_author(document, number)
+    document.xpath("//table[#{number}][@class='table_gl']//td[@class='span_str']//a[2]//strong").text
+  end
+
+  def self.get_description(document, number)
+    document.xpath("//table[#{number}][@class='table_gl']//tr[3][@class='td_center_color']//td/p").text
+  end
+
+  def self.get_publishing(document, number)
+    publishing = document.xpath("//table[#{number}][@class='table_gl']//td[@class='span_str']//p/text()").text
+    publishing.delete("\t\r").strip!.squeeze("\n").split("\n")
+  end
+
+  def self.get_image(document, number)
+    'http://loveread.ec/' + document.xpath("//table[#{number}][@class='table_gl']//img[@class='margin-right_8']").attr('src')
+  end
 end
 
-puts(global)
-time2 = Time.now
-puts(time2 - time1)
+class Scraper
+  @@threads = []
+  @@mutex = Mutex.new
+  @@books = []
+
+  def self.initialize
+    @books = []
+  end
+
+  def get_information(url)
+    (1..2).each do |page|
+      @@threads << Thread.new(page) do
+        document = Download.get_html(url, page)
+        Scraper.get_information_page(document)
+      end
+      @@threads.each(&:join)
+    end
+    @@books
+  end
+
+  def self.get_information_page(document)
+    (1..6).each do |number|
+      name = Download.get_name(document, number)
+      genre = Download.get_genre(document, number)
+      author = Download.get_author(document, number)
+      description = Download.get_description(document, number)
+      publishing = Download.get_publishing(document, number)
+      image = Download.get_image(document, number)
+      @@books << Scraper.write(name, genre, author, description, publishing, image)
+    end
+  end
+
+  def self.write(name, genre, author, description, publishing, image)
+    @@mutex.synchronize do
+      [
+        name, genre, author,
+        publishing[0], publishing[1],
+        publishing[2], publishing[3],
+        publishing[4], publishing[5],
+        description, image
+      ]
+    end
+  end
+end
+
+start = Scraper.new
+start.get_information(url)

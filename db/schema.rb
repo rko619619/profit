3c66b8aca216d96d3d54fcc03b0c240787e9fa29
Yanis Romanovich <thrasherdota2@gmail.com>
2020-12-25 00:55:27 +0300

    Add a some settings for database

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,4 @@
 
 ActiveRecord::Schema.define(version: 2020_12_24_213936) do
 
-  create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "title"
-    t.string "string"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end

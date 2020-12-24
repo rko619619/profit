1bc16e63989070ee48a6415c29f9ba7fc2a0f1c2
Yanis Romanovich <thrasherdota2@gmail.com>
2020-12-25 00:42:19 +0300

    Settings

--- db/schema.rb
+++ db/schema.rb
@@ -9,4 +9,15 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-#
+
+ActiveRecord::Schema.define(version: 2020_12_24_213936) do
+
+  create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.string "string"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end

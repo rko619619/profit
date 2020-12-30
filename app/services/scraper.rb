d295b4d74e10f469c40ed93680746018f0439e90
Yanis Romanovich <thrasherdota2@gmail.com>
2020-12-30 10:58:36 +0300

    asd

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -31,7 +31,7 @@ class Scraper
 
   def category_id
     category = doc.xpath("//table[#{num}][@class='table_gl']//tr[@class='td_top_color']//td[1]//p").text
-    Category.find_by(name: category).id
+    puts(Category.find_by(name: category))
   end
 
   def author

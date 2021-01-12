9a4f1a06c7cdb7563128b2c2c1998144a19b2c46
Yanis Romanovich <thrasherdota2@gmail.com>
2021-01-13 00:12:14 +0300

    Added a ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,4 @@
 
 class ApplicationController < ActionController::Base
   include Pagy::Backend
-
-  before_filter do
-    if request.ssl? && Rails.env.production?
-      redirect_to :protocol => 'http://', :status => :moved_permanently
-    end
-  end
-
 end

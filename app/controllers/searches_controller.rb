f8338ba41cf13f6225731d34edea3c6674c8376d
Yanis Romanovich <thrasherdota2@gmail.com>
2021-01-13 18:07:40 +0300

    Clean version of search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class SearchesController < ApplicationController
   def index
-    @books = Book.search(params[:query], fields: [:name, :author, :language])
+    @books = Book.search(params[:query], fields: %i[name author isbn])
   end
 end
